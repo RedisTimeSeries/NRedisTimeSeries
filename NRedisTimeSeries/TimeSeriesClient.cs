2f41b1380ab0bf8f56c13d8704667d809709ba2a
DvirDukhan <dvir@redislabs.com>
2020-06-16 10:04:09 +0300

    added missing comments

--- NRedisTimeSeries/TimeSeriesClient.cs
+++ NRedisTimeSeries/TimeSeriesClient.cs
@@ -155,7 +155,7 @@ namespace NRedisTimeSeries
         }
 
         /// <summary>
-        /// 
+        /// Deletes a compaction rule.
         /// </summary>
         /// <param name="db">StackExchange.Redis IDatabase instance</param>
         /// <param name="sourceKey">Key name for source time series</param>
@@ -199,7 +199,7 @@ namespace NRedisTimeSeries
         }
 
         /// <summary>
-        /// 
+        /// Query a range.
         /// </summary>
         /// <param name="db">StackExchange.Redis IDatabase instance</param>
         /// <param name="key">Key name for timeseries</param>
@@ -219,7 +219,7 @@ namespace NRedisTimeSeries
         }
 
         /// <summary>
-        /// 
+        /// Query a timestamp range across multiple time-series by filters.
         /// </summary>
         /// <param name="db">StackExchange.Redis IDatabase instance</param>
         /// <param name="fromTimeStamp"> Start timestamp for the range query. - can be used to express the minimum possible timestamp.</param>
@@ -229,7 +229,7 @@ namespace NRedisTimeSeries
         /// <param name="aggregation">Optional: Aggregation type</param>
         /// <param name="timeBucket">Optional: Time bucket for aggregation in milliseconds</param>
         /// <param name="withLabels">Optional: Include in the reply the label-value pairs that represent metadata labels of the time-series</param>
-        /// <returns></returns>
+        /// <returns>A list of <(key, labels, values)> tuples. Each tuple contains the key name, its labels and the values which satisfies the given range and filters.</returns>
         public static IReadOnlyList<(string key, IReadOnlyList<TimeSeriesLabel> labels, IReadOnlyList<TimeSeriesTuple> values)> TimeSeriesMRange(this IDatabase db, TimeStamp fromTimeStamp, TimeStamp toTimeStamp, IReadOnlyCollection<string> filter, long? count = null, Aggregation aggregation = null, long? timeBucket = null, bool? withLabels = null)
         {
             var args = new List<object>() { fromTimeStamp.Value, toTimeStamp.Value };
