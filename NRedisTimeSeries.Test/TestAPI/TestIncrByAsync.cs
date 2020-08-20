a5233bc9f2e25d39acfc25ae9b04f1c58f930238
filipecosta90 <filipecosta.90@gmail.com>
2020-08-20 13:05:49 +0100

    [fix] Fixed negative tests error reply comparison

--- NRedisTimeSeries.Test/TestAPI/TestIncrByAsync.cs
+++ NRedisTimeSeries.Test/TestAPI/TestIncrByAsync.cs
@@ -98,10 +98,10 @@ namespace NRedisTimeSeries.Test.TestAPI
             var value = 5.5;
             var db = redisFixture.Redis.GetDatabase();
             var ex = await Assert.ThrowsAsync<RedisServerException>(async () => await db.TimeSeriesIncrByAsync(key, value, timestamp: "+"));
-            Assert.Equal("TSDB: invalid timestamp", ex.Message);
+            Assert.Equal("ERR TSDB: invalid timestamp", ex.Message);
 
             ex = await Assert.ThrowsAsync<RedisServerException>(async () => await db.TimeSeriesIncrByAsync(key, value, timestamp: "-"));
-            Assert.Equal("TSDB: invalid timestamp", ex.Message);
+            Assert.Equal("ERR TSDB: invalid timestamp", ex.Message);
         }
     }
 }
