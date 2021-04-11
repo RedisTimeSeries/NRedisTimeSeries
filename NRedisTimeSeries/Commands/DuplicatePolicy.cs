eadfa667bcd0b45c5e149dabbdab19a70c70d115
Avital Fine <79420960+AvitalFineRedis@users.noreply.github.com>
2021-04-11 19:54:21 +0300

    Update DuplicatePolicy.cs

--- NRedisTimeSeries/Commands/DuplicatePolicy.cs
+++ NRedisTimeSeries/Commands/DuplicatePolicy.cs
@@ -3,32 +3,32 @@ namespace NRedisTimeSeries.Commands
 { 
     /// <summary>
     /// Policy to handle duplicate samples. 
-    /// The default policy for database-wide is BLOCK
+    /// The default policy for database-wide is BLOCK.
     /// </summary>
     public enum TsDuplicatePolicy
     {
         /// <summary>
-        /// an error will occur for any out of order sample
+        /// An error will occur for any out of order sample.
         /// </summary>
         BLOCK,
 
         /// <summary>
-        /// ignore the new value
+        /// Ignore the new value.
         /// </summary>
         FIRST,
 
         /// <summary>
-        /// override with latest value
+        /// Override with latest value.
         /// </summary>
         LAST,
 
         /// <summary>
-        /// only override if the value is lower than the existing value
+        /// Only override if the value is lower than the existing value.
         /// </summary>
         MIN,
 
         /// <summary>
-        /// only override if the value is higher than the existing value
+        /// Only override if the value is higher than the existing value.
         /// </summary>
         MAX,
 
